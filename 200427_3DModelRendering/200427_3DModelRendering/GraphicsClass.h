1e5a7315dfaa85a11842f8e0ad96951d97a8c8c0
HYEONe2 <31698845+HYEONe2@users.noreply.github.com>
2020-05-29 18:49:38 +0900

    200427_3DModelRendering
    .

---
+++ 200427_3DModelRendering/200427_3DModelRendering/GraphicsClass.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <windows.h>
+#include "D3DClass.h"
+
+#include "CameraClass.h"
+#include "ModelClass.h"
+#include "ColorShaderClass.h"
+#include "TextureShaderClass.h"
+
+// Globals
+const bool FULL_SCREEN = false;
+// 주사율을 맞춰주는 것
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+	
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+	bool Render(float);
+private:
+	D3DClass* m_D3D;
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	ColorShaderClass* m_ColorShader;
+	TextureShaderClass* m_TextureShader;
+};
